cd21e44819ca3b6164028ac73878d596b5041675
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-04-03 07:06:19 -0300

    1.0.1a2

--- LevelUP/Server/LevelHunter.cs
+++ LevelUP/Server/LevelHunter.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using System.Text.Json;
 using HarmonyLib;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
 
 namespace LevelUP.Server;
@@ -85,10 +88,60 @@ class LevelHunter
     }
 
     // Override Damage Interaction
-    [HarmonyPostfix]
+    [HarmonyPrefix]
     [HarmonyPatch(typeof(Entity), "ReceiveDamage")]
-    public static void IncreaseDamage(Entity __instance, DamageSource damageSource, EntityAgent targetEntity)
+    public static bool ReceiveDamage(Entity __instance, DamageSource damageSource, float damage)
     {
-        
+        damage *= 4;
+        if ((!__instance.Alive || __instance.IsActivityRunning("invulnerable")) && damageSource.Type != EnumDamageType.Heal)
+        {
+            return false;
+        }
+        if (__instance.ShouldReceiveDamage(damageSource, damage))
+        {
+            foreach (EntityBehavior behavior in __instance.SidedProperties.Behaviors)
+            {
+                behavior.OnEntityReceiveDamage(damageSource, ref damage);
+            }
+            if (damageSource.Type != EnumDamageType.Heal && damage > 0f)
+            {
+                __instance.WatchedAttributes.SetInt("onHurtCounter", __instance.WatchedAttributes.GetInt("onHurtCounter") + 1);
+                __instance.WatchedAttributes.SetFloat("onHurt", damage);
+                if (damage > 0.05f)
+                {
+                    __instance.AnimManager.StartAnimation("hurt");
+                }
+            }
+            if (damageSource.GetSourcePosition() != null)
+            {
+                Vec3d dir = (__instance.SidedPos.XYZ - damageSource.GetSourcePosition()).Normalize();
+                dir.Y = 0.699999988079071;
+                float factor = damageSource.KnockbackStrength * GameMath.Clamp((1f - __instance.Properties.KnockbackResistance) / 10f, 0f, 1f);
+                __instance.WatchedAttributes.SetFloat("onHurtDir", (float)Math.Atan2(dir.X, dir.Z));
+                __instance.WatchedAttributes.SetDouble("kbdirX", dir.X * (double)factor);
+                __instance.WatchedAttributes.SetDouble("kbdirY", dir.Y * (double)factor);
+                __instance.WatchedAttributes.SetDouble("kbdirZ", dir.Z * (double)factor);
+            }
+            else
+            {
+                __instance.WatchedAttributes.SetDouble("kbdirX", 0.0);
+                __instance.WatchedAttributes.SetDouble("kbdirY", 0.0);
+                __instance.WatchedAttributes.SetDouble("kbdirZ", 0.0);
+                __instance.WatchedAttributes.SetFloat("onHurtDir", -999f);
+            }
+            return damage > 0f;
+        }
+        return false;
     }
+
+    // // Override Damage Interaction
+    // [HarmonyTranspiler]
+    // [HarmonyPatch(typeof(EntityAgent), "ReceiveDamage")]
+    // public static IEnumerable<CodeInstruction> ReceiveDamage(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    // {
+    //     return new CodeMatcher(instructions, generator)
+    //         .MatchEndForward(new CodeMatch(OpCodes.Ldarg_1, null, "damage"))
+    //         .SetOperandAndAdvance(0.0f)
+    //         .InstructionEnumeration();
+    // }
 }
