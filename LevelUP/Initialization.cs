efd23fc0d57c42b7bcdf1696f10adc5796081744
LeansBoboDev <leans.org@proton.me>
2024-07-19 13:59:31 -0300

    1.2.5-pre.1³

--- LevelUP/Initialization.cs
+++ LevelUP/Initialization.cs
@@ -66,18 +66,11 @@ public class Initialization : ModSystem
 
 public class Debug
 {
-    private static readonly OperatingSystem system = Environment.OSVersion;
-    static private ILogger loggerForNonTerminalUsers;
+    static private ILogger logger;
 
-    static public void LoadLogger(ILogger logger) => loggerForNonTerminalUsers = logger;
+    static public void LoadLogger(ILogger _logger) => logger = _logger;
     static public void Log(string message)
     {
-        // Check if is linux or other based system and if the terminal is active for the logs to be show
-        if ((system.Platform == PlatformID.Unix || system.Platform == PlatformID.Other) && Environment.UserInteractive)
-            // Based terminal users
-            Console.WriteLine($"{DateTime.Now:d.M.yyyy HH:mm:ss} [LevelUP] {message}");
-        else
-            // Unbased non terminal users
-            loggerForNonTerminalUsers?.Log(EnumLogType.Notification, $"[LevelUP] {message}");
+        logger?.Log(EnumLogType.Notification, $"[LevelUP] {message}");
     }
 }
