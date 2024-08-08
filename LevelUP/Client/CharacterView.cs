ab3572046c9ea6f0a325d7925b387856eac77b25
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-08-08 10:57:15 -0300

    1.2.6-pre.1

--- LevelUP/Client/CharacterView.cs
+++ LevelUP/Client/CharacterView.cs
@@ -1,7 +1,6 @@
-using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Client;
-using Vintagestory.API.Config;
+using Vintagestory.API.Common;
 
 namespace LevelUP.Client;
 
@@ -10,6 +9,9 @@ class CharacterView
     Instance instance;
 
     GuiDialogCharacterBase characterView;
+    private GuiComposer levelTabComposer;
+
+    byte page = 0;
 
     public void Init(Instance _instance)
     {
@@ -31,45 +33,151 @@ class CharacterView
         });
     }
 
-    // Compose the gui page for the level tab
     private void ComposeLevelTab(GuiComposer composer)
     {
-        composer.AddRichtext(GetText(), CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15), ElementBounds.Fixed(0.0, 25.0, 385.0, 200.0));
+        switch (page)
+        {
+            case 0: ComposeFirstPage(composer); break;
+
+            case 1: break;
+        }
+        composer.AddButton("Back", OnBackClicked, ElementBounds.Fixed(0.0, 300.0, 20.0, 15.0));
+        composer.AddButton("Next", OnNextClicked, ElementBounds.Fixed(310.0, 300.0, 30.0, 15.0));
+
+        levelTabComposer = composer;
+        // #region 3
+        // // Leather Armor
+        // composer.AddImage(ElementBounds.Fixed(0.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:leather_armor")}: {GetLevelByLevelName("LeatherArmor")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(0.0, 300.0, 385.0, 200.0)
+        // );
+        // // Chain Armor
+        // composer.AddImage(ElementBounds.Fixed(100.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:chain_armor")}: {GetLevelByLevelName("ChainArmor")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(100.0, 300.0, 385.0, 200.0)
+        // );
+        // // Hunter
+        // composer.AddImage(ElementBounds.Fixed(200.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:hunter")}: {GetLevelByLevelName("Hunter")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(200.0, 300.0, 385.0, 200.0)
+        // );
+        // // Farming
+        // composer.AddImage(ElementBounds.Fixed(300.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:farming")}: {GetLevelByLevelName("Farming")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(300.0, 300.0, 385.0, 200.0)
+        // );
+        // #endregion
+        // #region 4
+        // // Cooking
+        // composer.AddImage(ElementBounds.Fixed(0.0, 325.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:cooking")}: {GetLevelByLevelName("Cooking")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(0.0, 400.0, 385.0, 200.0)
+        // );
+        // // Vitality
+        // composer.AddImage(ElementBounds.Fixed(100.0, 325.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        // composer.AddRichtext(
+        //     $"{Lang.Get("levelup:vitality")}: {GetLevelByLevelName("Vitality")}",
+        //     CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15),
+        //     ElementBounds.Fixed(100.0, 400.0, 385.0, 200.0)
+        // );
+        // #endregion
     }
 
-    // Creates the full text for level tab
-    private string GetText()
+    private void ComposeFirstPage(GuiComposer composer)
     {
-        int hunterLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Hunter");
-        int bowLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Bow");
-        int knifeLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Knife");
-        int spearLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Spear");
-        int axeLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Axe");
-        int pickaxeLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Pickaxe");
-        int shovelLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Shovel");
-        int hammerLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Hammer");
-        int swordLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Sword");
-        int shieldLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Shield");
-        int farmingLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Farming");
-        int cookingLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Cooking");
-        int vitalityLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_Vitality");
-        int leatherArmorLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_LeatherArmor");
-        int chainArmorLevel = instance.api.World.Player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ChainArmor");
+        #region 1
+        // Axe
+        composer.AddImage(ElementBounds.Fixed(0.0, 25.0, 385.0, 200.0), new AssetLocation("levelup:axe.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(0.0, 25.0, 385.0, 200.0), GetLevelByLevelName("Axe"));
+        // Pickaxe
+        composer.AddImage(ElementBounds.Fixed(150.0, 25.0, 385.0, 200.0), new AssetLocation("levelup:pickaxe.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(150.0, 25.0, 385.0, 200.0), GetLevelByLevelName("Pickaxe"));
+        // Shovel
+        composer.AddImage(ElementBounds.Fixed(300.0, 25.0, 385.0, 200.0), new AssetLocation("levelup:shovel.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(300.0, 25.0, 385.0, 200.0), GetLevelByLevelName("Shovel"));
+        #endregion
+        #region 2
+        // Knife
+        composer.AddImage(ElementBounds.Fixed(0.0, 125.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(0.0, 125.0, 385.0, 200.0), GetLevelByLevelName("Knife"));
+        // Bow
+        composer.AddImage(ElementBounds.Fixed(150.0, 125.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(150.0, 125.0, 385.0, 200.0), GetLevelByLevelName("Bow"));
+        // Spear
+        composer.AddImage(ElementBounds.Fixed(300.0, 125.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(300.0, 125.0, 385.0, 200.0), GetLevelByLevelName("Spear"));
+        #endregion
+        #region 3
+        // Hammer
+        composer.AddImage(ElementBounds.Fixed(0.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(0.0, 225.0, 385.0, 200.0), GetLevelByLevelName("Hammer"));
+        // Sword
+        composer.AddImage(ElementBounds.Fixed(150.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(150.0, 225.0, 385.0, 200.0), GetLevelByLevelName("Sword"));
+        // Shield
+        composer.AddImage(ElementBounds.Fixed(300.0, 225.0, 385.0, 200.0), new AssetLocation("levelup:unknown.png"));
+        SetComposerLevel(composer, ElementBounds.Fixed(300.0, 225.0, 385.0, 200.0), GetLevelByLevelName("Shield"));
+        #endregion
+    }
 
-        StringBuilder fulldesc = new();
-        fulldesc.AppendLine(Lang.Get("levelup:character_levels"));
-        fulldesc.AppendLine($"-- {Lang.Get("levelup:tools_levels")} --");
-        fulldesc.AppendLine($"{Lang.Get("levelup:axe")}: {axeLevel}   {Lang.Get("levelup:pickaxe")}: {pickaxeLevel}   {Lang.Get("levelup:shovel")}: {shovelLevel}");
-        fulldesc.AppendLine($"{Lang.Get("levelup:bow")}: {bowLevel}   {Lang.Get("levelup:spear")}: {spearLevel}   {Lang.Get("levelup:knife")}: {knifeLevel}");
-        fulldesc.AppendLine($"{Lang.Get("levelup:armors")}: {hammerLevel}   {Lang.Get("levelup:sword")}: {swordLevel}   {Lang.Get("levelup:shield")}: {shieldLevel}");
-        fulldesc.AppendLine($"-- {Lang.Get("levelup:armor_levels")} --");
-        fulldesc.AppendLine($"{Lang.Get("levelup:leather_armor")}: {leatherArmorLevel}");
-        fulldesc.AppendLine($"{Lang.Get("chain_armor")}: {chainArmorLevel}");
-        fulldesc.AppendLine($"-- {Lang.Get("levelup:specialist_levels")} --");
-        fulldesc.AppendLine($"{Lang.Get("levelup:hunter")}: {hunterLevel}");
-        fulldesc.AppendLine($"{Lang.Get("levelup:farming")}: {farmingLevel}");
-        fulldesc.AppendLine($"{Lang.Get("levelup:cooking")}: {cookingLevel}");
-        fulldesc.AppendLine($"{Lang.Get("levelup:vitality")}: {vitalityLevel}");
-        return fulldesc.ToString();
+    private int GetLevelByLevelName(string levelName) => instance.api.World.Player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_{levelName}");
+
+    private static void SetComposerLevel(GuiComposer composer, ElementBounds position, int level)
+    {
+        // Single Digits
+        if (level < 10)
+            // Creating the imagem
+            composer.AddImage(position, new AssetLocation($"levelup:{level}.png"));
+        // Double Digits
+        else if (level < 100)
+        {
+            string levelStr = level.ToString();
+            int firstDigit = int.Parse(levelStr[0].ToString());
+            int secondDigit = int.Parse(levelStr[1].ToString());
+
+            // First digit instanciation
+            ElementBounds firstDigitPosition = ElementBounds.Fixed(
+                position.fixedX - 15,
+                position.fixedY,
+                position.fixedWidth,
+                position.fixedHeight
+            );
+            composer.AddImage(firstDigitPosition, new AssetLocation($"levelup:{firstDigit}.png"));
+
+            // Second digit instanciation
+            ElementBounds secondDigitPosition = ElementBounds.Fixed(
+                position.fixedX + 15,
+                position.fixedY,
+                position.fixedWidth,
+                position.fixedHeight
+            );
+            composer.AddImage(secondDigitPosition, new AssetLocation($"levelup:{secondDigit}.png"));
+        }
+
+        Debug.Log("Ops, you reached the gui limit :P            ");
+    }
+    
+    private bool OnBackClicked()
+    {
+        page--;
+        if (page < 0) page = 0;
+        return true;
+    }
+
+    private bool OnNextClicked()
+    {
+        page++;
+        if (page > 1) page = 1;
+        return true;
     }
 }
