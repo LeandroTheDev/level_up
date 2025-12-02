5f0e36f2bb29fae1b85b84e1c1495e0196771bff
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-12-02 09:36:06 -0300

    3.0.0-pre.16¹

--- LevelUP/Server/LevelBrigandineArmor.cs
+++ LevelUP/Server/LevelBrigandineArmor.cs
@@ -31,7 +31,6 @@ class LevelBrigandineArmor
         Configuration.RegisterNewLevel("BrigandineArmor");
         Configuration.RegisterNewLevelTypeEXP("BrigandineArmor", Configuration.BrigandineArmorGetLevelByEXP);
         Configuration.RegisterNewEXPLevelType("BrigandineArmor", Configuration.BrigandineArmorGetExpByLevel);
-        // Update armor stats before functions
         OverwriteDamageInteractionEvents.OnPlayerArmorReceiveHandleStats += StatsUpdated;
         OverwriteDamageInteractionEvents.OnPlayerArmorReceiveDamageStat += DamageReceived;
 
@@ -54,35 +53,160 @@ class LevelBrigandineArmor
         StatusViewEvents.OnStatusRequested -= StatusViewRequested;
     }
 
+    public readonly Dictionary<string, string> SubLevelPatterns = new()
+    {
+        { "-copper-", "Copper" },
+        { "-tinbronze-", "TinBronze" },
+        { "-bismuthbronze-", "BismuthBronze" },
+        { "-blackbronze-", "BlackBronze" },
+        { "-iron-", "Iron" },
+        { "-meteoriciron-", "MeteoricIron" },
+        { "-steel-", "Steel" }
+    };
+
     private void StatusViewRequested(IPlayer player, ref StringBuilder stringBuilder, string levelType)
     {
         if (levelType != "BrigandineArmor") return;
 
         stringBuilder.AppendLine(
-            Lang.Get("levelup:status_statsincreaser",
-                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            Lang.Get("levelup:status_relativeprotection",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
             )
         );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_flatdamagereduction",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_healingeffectivness",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorHealingEffectivnessMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_hungerrate",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorHungerRateMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_rangedaccuracy",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorRangedWeaponsAccuracyMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_rangedspeed",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorRangedWeaponsSpeedMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_walkspeed",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.BrigandineArmorWalkSpeedMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine("");
+
+        stringBuilder.AppendLine(Lang.Get("levelup:status_proficiency"));
+
+        foreach (var pair in SubLevelPatterns)
+        {
+            stringBuilder.AppendLine($"{Lang.Get($"levelup:{pair.Value.ToLower()}")}: {player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}")}");
+        }
     }
 
-    private void ViewReceived(IPlayer player, ItemSlot item)
+    private void ViewReceived(IPlayer player, ItemSlot armorSlot)
     {
-        if (!Configuration.expMultiplyHitBrigandineArmor.ContainsKey(item.Itemstack.Collectible.Code)) return;
-        float statusIncrease = Configuration.BrigandineArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor"));
-        Shared.Instance.RefreshArmorAttributes(item, statusIncrease);
-        LevelBrigandineArmorEvents.ExecuteItemInfoUpdated(item.Itemstack.Item as ItemWearable, player);
+        if (!Configuration.expMultiplyHitBrigandineArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) return;
+
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor");
+        float relativeProtection = Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerLevel);
+        float flatDamageReduction = Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+        float healingEffectivness = Configuration.BrigandineArmorHealingEffectivnessMultiplyByLevel(playerLevel);
+        float hungerRate = Configuration.BrigandineArmorHungerRateMultiplyByLevel(playerLevel);
+        float rangedWeaponsAccuracy = Configuration.BrigandineArmorRangedWeaponsAccuracyMultiplyByLevel(playerLevel);
+        float rangedWeaponsSpeed = Configuration.BrigandineArmorRangedWeaponsSpeedMultiplyByLevel(playerLevel);
+        float walkSpeed = Configuration.BrigandineArmorWalkSpeedMultiplyByLevel(playerLevel);
+
+        string code = armorSlot.Itemstack.Item.Code.ToString();
+        foreach (var pair in SubLevelPatterns)
+        {
+            if (code.Contains(pair.Key))
+            {
+                int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                relativeProtection += Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                flatDamageReduction += Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                healingEffectivness += Configuration.BrigandineArmorHealingEffectivnessMultiplyByLevel(playerSubLevel) - 1f;
+                hungerRate += Configuration.BrigandineArmorHungerRateMultiplyByLevel(playerSubLevel) - 1f;
+                rangedWeaponsAccuracy += Configuration.BrigandineArmorRangedWeaponsAccuracyMultiplyByLevel(playerSubLevel) - 1f;
+                rangedWeaponsSpeed += Configuration.BrigandineArmorRangedWeaponsSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                walkSpeed += Configuration.BrigandineArmorWalkSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                break;
+            }
+        }
+
+        Shared.Instance.RefreshArmorAttributes(
+                armorSlot,
+                relativeProtection,
+                flatDamageReduction,
+                healingEffectivness,
+                hungerRate,
+                rangedWeaponsAccuracy,
+                rangedWeaponsSpeed,
+                walkSpeed);
+
+        LevelBrigandineArmorEvents.ExecuteItemInfoUpdated(armorSlot.Itemstack.Item as ItemWearable, player);
     }
 
     private void StatsUpdated(IPlayer player, List<ItemSlot> items)
     {
-        float statusIncrease = Configuration.BrigandineArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor"));
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor");
+
         foreach (ItemSlot armorSlot in items)
         {
             if (!Configuration.expMultiplyHitBrigandineArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) continue;
 
             ItemWearable armor = armorSlot.Itemstack.Item as ItemWearable;
 
-            Shared.Instance.RefreshArmorAttributes(armorSlot, statusIncrease);
+            float relativeProtection = Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerLevel);
+            float flatDamageReduction = Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+            float healingEffectivness = Configuration.BrigandineArmorHealingEffectivnessMultiplyByLevel(playerLevel);
+            float hungerRate = Configuration.BrigandineArmorHungerRateMultiplyByLevel(playerLevel);
+            float rangedWeaponsAccuracy = Configuration.BrigandineArmorRangedWeaponsAccuracyMultiplyByLevel(playerLevel);
+            float rangedWeaponsSpeed = Configuration.BrigandineArmorRangedWeaponsSpeedMultiplyByLevel(playerLevel);
+            float walkSpeed = Configuration.BrigandineArmorWalkSpeedMultiplyByLevel(playerLevel);
+
+            string code = armor.Code.ToString();
+            foreach (var pair in SubLevelPatterns)
+            {
+                if (code.Contains(pair.Key))
+                {
+                    int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                    relativeProtection += Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                    flatDamageReduction += Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                    healingEffectivness += Configuration.BrigandineArmorHealingEffectivnessMultiplyByLevel(playerSubLevel) - 1f;
+                    hungerRate += Configuration.BrigandineArmorHungerRateMultiplyByLevel(playerSubLevel) - 1f;
+                    rangedWeaponsAccuracy += Configuration.BrigandineArmorRangedWeaponsAccuracyMultiplyByLevel(playerSubLevel) - 1f;
+                    rangedWeaponsSpeed += Configuration.BrigandineArmorRangedWeaponsSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                    walkSpeed += Configuration.BrigandineArmorWalkSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                    break;
+                }
+            }
+
+            Shared.Instance.RefreshArmorAttributes(
+                armorSlot,
+                relativeProtection,
+                flatDamageReduction,
+                healingEffectivness,
+                hungerRate,
+                rangedWeaponsAccuracy,
+                rangedWeaponsSpeed,
+                walkSpeed);
 
             LevelBrigandineArmorEvents.ExecuteItemHandledStats(armor, player);
         }
@@ -90,18 +214,33 @@ class LevelBrigandineArmor
 
     private void DamageReceived(IPlayer player, List<ItemSlot> items, ref float damage)
     {
-        float statusIncrease = Configuration.BrigandineArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor"));
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_BrigandineArmor");
         foreach (ItemSlot armorSlot in items)
         {
             if (!Configuration.expMultiplyHitBrigandineArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) continue;
 
             ItemWearable armor = armorSlot.Itemstack.Item as ItemWearable;
 
+            float relativeProtection = Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerLevel);
+            float flatDamageReduction = Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+
             double multiply = Configuration.expMultiplyHitBrigandineArmor[armor.Code];
             ulong exp = (ulong)(Configuration.BrigandineArmorBaseEXPEarnedByDAMAGE(damage) * multiply);
             Experience.IncreaseExperience(player, "BrigandineArmor", exp);
+            string code = armor.Code.ToString();
+            foreach (var pair in SubLevelPatterns)
+            {
+                if (code.Contains(pair.Key))
+                {
+                    int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                    relativeProtection += Configuration.BrigandineArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                    flatDamageReduction += Configuration.BrigandineArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                    Experience.IncreaseSubExperience(player, "BrigandineArmor", pair.Value, exp);
+                    break;
+                }
+            }
 
-            Shared.Instance.RefreshArmorAttributes(armorSlot, statusIncrease);
+            Shared.Instance.RefreshArmorAttributes(armorSlot, relativeProtection, flatDamageReduction);
 
             LevelBrigandineArmorEvents.ExecuteItemHandledDamage(armor, player);
         }
--- LevelUP/Server/LevelScaleArmor.cs
+++ LevelUP/Server/LevelScaleArmor.cs
@@ -31,7 +31,6 @@ class LevelScaleArmor
         Configuration.RegisterNewLevel("ScaleArmor");
         Configuration.RegisterNewLevelTypeEXP("ScaleArmor", Configuration.ScaleArmorGetLevelByEXP);
         Configuration.RegisterNewEXPLevelType("ScaleArmor", Configuration.ScaleArmorGetExpByLevel);
-        // Update armor stats before functions
         OverwriteDamageInteractionEvents.OnPlayerArmorReceiveHandleStats += StatsUpdated;
         OverwriteDamageInteractionEvents.OnPlayerArmorReceiveDamageStat += DamageReceived;
 
@@ -54,35 +53,156 @@ class LevelScaleArmor
         StatusViewEvents.OnStatusRequested -= StatusViewRequested;
     }
 
+    public readonly Dictionary<string, string> SubLevelPatterns = new()
+    {
+        { "-sewn-", "Sewn" },
+        { "-jerkin-", "Jerkin" },
+        { "-bear-", "Bear" }
+    };
+
     private void StatusViewRequested(IPlayer player, ref StringBuilder stringBuilder, string levelType)
     {
         if (levelType != "ScaleArmor") return;
 
         stringBuilder.AppendLine(
-            Lang.Get("levelup:status_statsincreaser",
-                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            Lang.Get("levelup:status_relativeprotection",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
             )
         );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_flatdamagereduction",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_healingeffectivness",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorHealingEffectivnessMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_hungerrate",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorHungerRateMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_rangedaccuracy",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorRangedWeaponsAccuracyMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_rangedspeed",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorRangedWeaponsSpeedMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine(
+            Lang.Get("levelup:status_walkspeed",
+                Utils.GetPorcentageFromFloatsStart1(Configuration.ScaleArmorWalkSpeedMultiplyByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor")))
+            )
+        );
+
+        stringBuilder.AppendLine("");
+
+        stringBuilder.AppendLine(Lang.Get("levelup:status_proficiency"));
+
+        foreach (var pair in SubLevelPatterns)
+        {
+            stringBuilder.AppendLine($"{Lang.Get($"levelup:{pair.Value.ToLower()}")}: {player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}")}");
+        }
     }
 
-    private void ViewReceived(IPlayer player, ItemSlot item)
+    private void ViewReceived(IPlayer player, ItemSlot armorSlot)
     {
-        if (!Configuration.expMultiplyHitScaleArmor.ContainsKey(item.Itemstack.Collectible.Code)) return;
-        float statusIncrease = Configuration.ScaleArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor"));
-        Shared.Instance.RefreshArmorAttributes(item, statusIncrease);
-        LevelScaleArmorEvents.ExecuteItemInfoUpdated(item.Itemstack.Item as ItemWearable, player);
+        if (!Configuration.expMultiplyHitScaleArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) return;
+
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor");
+        float relativeProtection = Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerLevel);
+        float flatDamageReduction = Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+        float healingEffectivness = Configuration.ScaleArmorHealingEffectivnessMultiplyByLevel(playerLevel);
+        float hungerRate = Configuration.ScaleArmorHungerRateMultiplyByLevel(playerLevel);
+        float rangedWeaponsAccuracy = Configuration.ScaleArmorRangedWeaponsAccuracyMultiplyByLevel(playerLevel);
+        float rangedWeaponsSpeed = Configuration.ScaleArmorRangedWeaponsSpeedMultiplyByLevel(playerLevel);
+        float walkSpeed = Configuration.ScaleArmorWalkSpeedMultiplyByLevel(playerLevel);
+
+        string code = armorSlot.Itemstack.Item.Code.ToString();
+        foreach (var pair in SubLevelPatterns)
+        {
+            if (code.Contains(pair.Key))
+            {
+                int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                relativeProtection += Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                flatDamageReduction += Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                healingEffectivness += Configuration.ScaleArmorHealingEffectivnessMultiplyByLevel(playerSubLevel) - 1f;
+                hungerRate += Configuration.ScaleArmorHungerRateMultiplyByLevel(playerSubLevel) - 1f;
+                rangedWeaponsAccuracy += Configuration.ScaleArmorRangedWeaponsAccuracyMultiplyByLevel(playerSubLevel) - 1f;
+                rangedWeaponsSpeed += Configuration.ScaleArmorRangedWeaponsSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                walkSpeed += Configuration.ScaleArmorWalkSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                break;
+            }
+        }
+
+        Shared.Instance.RefreshArmorAttributes(
+                armorSlot,
+                relativeProtection,
+                flatDamageReduction,
+                healingEffectivness,
+                hungerRate,
+                rangedWeaponsAccuracy,
+                rangedWeaponsSpeed,
+                walkSpeed);
+
+        LevelScaleArmorEvents.ExecuteItemInfoUpdated(armorSlot.Itemstack.Item as ItemWearable, player);
     }
 
     private void StatsUpdated(IPlayer player, List<ItemSlot> items)
     {
-        float statusIncrease = Configuration.ScaleArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor"));
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor");
+
         foreach (ItemSlot armorSlot in items)
         {
             if (!Configuration.expMultiplyHitScaleArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) continue;
 
             ItemWearable armor = armorSlot.Itemstack.Item as ItemWearable;
 
-            Shared.Instance.RefreshArmorAttributes(armorSlot, statusIncrease);
+            float relativeProtection = Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerLevel);
+            float flatDamageReduction = Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+            float healingEffectivness = Configuration.ScaleArmorHealingEffectivnessMultiplyByLevel(playerLevel);
+            float hungerRate = Configuration.ScaleArmorHungerRateMultiplyByLevel(playerLevel);
+            float rangedWeaponsAccuracy = Configuration.ScaleArmorRangedWeaponsAccuracyMultiplyByLevel(playerLevel);
+            float rangedWeaponsSpeed = Configuration.ScaleArmorRangedWeaponsSpeedMultiplyByLevel(playerLevel);
+            float walkSpeed = Configuration.ScaleArmorWalkSpeedMultiplyByLevel(playerLevel);
+
+            string code = armor.Code.ToString();
+            foreach (var pair in SubLevelPatterns)
+            {
+                if (code.Contains(pair.Key))
+                {
+                    int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                    relativeProtection += Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                    flatDamageReduction += Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                    healingEffectivness += Configuration.ScaleArmorHealingEffectivnessMultiplyByLevel(playerSubLevel) - 1f;
+                    hungerRate += Configuration.ScaleArmorHungerRateMultiplyByLevel(playerSubLevel) - 1f;
+                    rangedWeaponsAccuracy += Configuration.ScaleArmorRangedWeaponsAccuracyMultiplyByLevel(playerSubLevel) - 1f;
+                    rangedWeaponsSpeed += Configuration.ScaleArmorRangedWeaponsSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                    walkSpeed += Configuration.ScaleArmorWalkSpeedMultiplyByLevel(playerSubLevel) - 1f;
+                    break;
+                }
+            }
+
+            Shared.Instance.RefreshArmorAttributes(
+                armorSlot,
+                relativeProtection,
+                flatDamageReduction,
+                healingEffectivness,
+                hungerRate,
+                rangedWeaponsAccuracy,
+                rangedWeaponsSpeed,
+                walkSpeed);
 
             LevelScaleArmorEvents.ExecuteItemHandledStats(armor, player);
         }
@@ -90,18 +210,33 @@ class LevelScaleArmor
 
     private void DamageReceived(IPlayer player, List<ItemSlot> items, ref float damage)
     {
-        float statusIncrease = Configuration.ScaleArmorStatsIncreaseByLevel(player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor"));
+        int playerLevel = player.Entity.WatchedAttributes.GetInt("LevelUP_Level_ScaleArmor");
         foreach (ItemSlot armorSlot in items)
         {
             if (!Configuration.expMultiplyHitScaleArmor.ContainsKey(armorSlot.Itemstack.Collectible.Code)) continue;
 
             ItemWearable armor = armorSlot.Itemstack.Item as ItemWearable;
 
+            float relativeProtection = Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerLevel);
+            float flatDamageReduction = Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerLevel);
+
             double multiply = Configuration.expMultiplyHitScaleArmor[armor.Code];
             ulong exp = (ulong)(Configuration.ScaleArmorBaseEXPEarnedByDAMAGE(damage) * multiply);
             Experience.IncreaseExperience(player, "ScaleArmor", exp);
+            string code = armor.Code.ToString();
+            foreach (var pair in SubLevelPatterns)
+            {
+                if (code.Contains(pair.Key))
+                {
+                    int playerSubLevel = player.Entity.WatchedAttributes.GetInt($"LevelUP_Level_Sub_{pair.Value}");
+                    relativeProtection += Configuration.ScaleArmorRelativeProtectionMultiplyByLevel(playerSubLevel) - 1f;
+                    flatDamageReduction += Configuration.ScaleArmorFlatDamageReductionMultiplyByLevel(playerSubLevel) - 1f;
+                    Experience.IncreaseSubExperience(player, "ScaleArmor", pair.Value, exp);
+                    break;
+                }
+            }
 
-            Shared.Instance.RefreshArmorAttributes(armorSlot, statusIncrease);
+            Shared.Instance.RefreshArmorAttributes(armorSlot, relativeProtection, flatDamageReduction);
 
             LevelScaleArmorEvents.ExecuteItemHandledDamage(armor, player);
         }
